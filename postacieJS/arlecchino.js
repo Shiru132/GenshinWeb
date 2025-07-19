7e077a5fbedc7558aa146f0b69e8314097e835ae
Shiru132 <111283000+Shiru132@users.noreply.github.com>
2025-07-19 23:15:49 +0200

    Update arlecchino.js

--- postacieJS/arlecchino.js
+++ postacieJS/arlecchino.js
@@ -278,45 +278,45 @@ function renderMainPic(mainName){
 
 }
 
- document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-            anchor.addEventListener('click', function(e) {
-                e.preventDefault();
-                const target = document.querySelector(this.getAttribute('href'));
-                if (target) {
-                    target.scrollIntoView({
-                        behavior: 'smooth',
-                        block: 'start'
-                    });
-                }
-            });
-        });
-const scrollBtn = document.querySelector('.scroll-to-top');
+//  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//             anchor.addEventListener('click', function(e) {
+//                 e.preventDefault();
+//                 const target = document.querySelector(this.getAttribute('href'));
+//                 if (target) {
+//                     target.scrollIntoView({
+//                         behavior: 'smooth',
+//                         block: 'start'
+//                     });
+//                 }
+//             });
+//         });
+// const scrollBtn = document.querySelector('.scroll-to-top');
 
-// showing when reach 300px
-window.addEventListener('scroll', () => {
-  if (window.pageYOffset > 300) {
-    scrollBtn.classList.add('show');
-  } else {
-    scrollBtn.classList.remove('show');
-  }
-});
+// // showing when reach 300px
+// window.addEventListener('scroll', () => {
+//   if (window.pageYOffset > 300) {
+//     scrollBtn.classList.add('show');
+//   } else {
+//     scrollBtn.classList.remove('show');
+//   }
+// });
 
-// smooth
-scrollBtn.addEventListener('click', () => {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-});
+// // smooth
+// scrollBtn.addEventListener('click', () => {
+//   window.scrollTo({ top: 0, behavior: 'smooth' });
+// });
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-            anchor.addEventListener('click', function(e) {
-                e.preventDefault();
-                const target = document.querySelector(this.getAttribute('href'));
-                if (target) {
-                    target.scrollIntoView({
-                        behavior: 'smooth',
-                        block: 'start'
-                    });
-                }
-            });
-        });
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//             anchor.addEventListener('click', function(e) {
+//                 e.preventDefault();
+//                 const target = document.querySelector(this.getAttribute('href'));
+//                 if (target) {
+//                     target.scrollIntoView({
+//                         behavior: 'smooth',
+//                         block: 'start'
+//                     });
+//                 }
+//             });
+//         });
 
 renderMainPic(mainName);
