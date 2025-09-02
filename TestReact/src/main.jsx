24136868e2ab95f5bd1ef41d7d19def460426a53
Filip Hołody <111283000+Shiru132@users.noreply.github.com>
2025-09-02 17:12:37 +0200

    Update main.jsx

--- TestReact/src/main.jsx
+++ TestReact/src/main.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
-import './pyro.css' // style sekcji postaci z public/CSS/
+import './pyro.css' 
 
 const root = createRoot(document.getElementById('root'))
 root.render(
